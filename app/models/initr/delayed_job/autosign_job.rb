8fc11d1eb5871702bcc02981f753e1cc400d2ddb
Lluis Gili <lluis@ingent.net>
2009-06-03 12:34:04 +0200

    log with puts in autosign_job, rename bad migration class name

--- app/models/initr/delayed_job/autosign_job.rb
+++ app/models/initr/delayed_job/autosign_job.rb
@@ -10,21 +10,20 @@ class Initr::DelayedJob::AutosignJob
     content = generate_content
     path = Setting.plugin_initr_plugin['autosign']
 
-    @log = RAILS_DEFAULT_LOGGER
     unless can_write? path
-      @log.error("Can't write file: #{path}")
+      puts "Can't write file: #{path}"
       return 1
     end
     lines=(`echo -n "#{content}" | wc -l`).to_i
     unless lines > 0
-      @log.error("Not overwriten file #{path} with empty content #{content}")
+      puts "Not overwriten file #{path} with empty content #{content}"
       return 1
     end
 
     File.open(path,"w") {|f| f << content}
     `chmod 644 #{path}`
     `chown apache: #{path}`
-    @log.info("Updated #{path}")
+    puts "Updated #{path}"
   end
 
   private
@@ -43,8 +42,8 @@ class Initr::DelayedJob::AutosignJob
     is_w_dir=( File.directory?(dir) and File.writable?(dir) )
     is_w_file=( !File.exist?(file) or File.writable?(file) )
     unless is_w_dir and is_w_dir
-      @log.debug("#{dir} is writable directory? [#{is_w_dir}]")
-      @log.debug("can write #{file}? [#{is_w_file}]")
+      puts "#{dir} is writable directory? [#{is_w_dir}]"
+      puts "can write #{file}? [#{is_w_file}]"
     end
     return (is_w_dir and is_w_file)
   end
