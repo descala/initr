c5d96d1d577d85559011f4f11dd41bd90db5cf9e
Lluis Gili <lluis@ingent.net>
2009-06-16 13:17:02 +0200

    validate uniqueness of name

--- app/models/initr/custom_klass_conf.rb
+++ app/models/initr/custom_klass_conf.rb
@@ -4,6 +4,7 @@ class Initr::CustomKlassConf < ActiveRecord::Base
 
   belongs_to :klass, :class_name => "Initr::CustomKlass"
   validates_presence_of :name, :value
+  validates_uniqueness_of :name, :scope => :custom_klass_id
 
   def value_yaml
     read_attribute("value")
