c56bbad75bed2c3527235fe9736d2a4e6ef5345f
David Escala <descala@ingent.net>
2009-08-07 15:08:41 +0200

    WPKG working on local development setup

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,3 +1,15 @@
 # site.pp
 
-# Everithing is on external nodes and modules
+# TODO: find a way to handle globals through a module
+
+# Default path
+Exec { path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" }
+
+# Default user for files
+File {
+  owner => "root",
+  group => "root"
+}
+
+# Manage root's crontab
+Cron { user => "root" }
