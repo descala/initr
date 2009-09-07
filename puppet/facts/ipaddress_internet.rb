f49589f1ede3d08a0aff59f63744d1cd4dbdc8dc
Lluis Gili <lluis@ingent.net>
2009-09-07 15:08:16 +0200

    get rid of 'already initialized constant IPADDRESS_URL' message

--- puppet/facts/ipaddress_internet.rb
+++ puppet/facts/ipaddress_internet.rb
@@ -2,8 +2,7 @@ require 'facter'
 require 'open-uri'
 
 Facter.add("ipaddress_internet") do
-  IPADDRESS_URL="http://#{Facter.value(:servername) || 'one.ingent.net'}/node/getip" 
   setcode do
-    open(IPADDRESS_URL).read.chomp
+    open("http://#{Facter.value(:servername) || 'one.ingent.net'}/node/getip").read.chomp
   end
 end
