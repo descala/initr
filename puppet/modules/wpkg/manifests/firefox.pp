a7f386dc08294796b65ec9a2099d6b920a9ea996
David Escala <descala@ingent.net>
2009-10-28 10:57:23 +0100

    wpkg firefox-3.5.4

--- puppet/modules/wpkg/manifests/firefox.pp
+++ puppet/modules/wpkg/manifests/firefox.pp
@@ -11,8 +11,8 @@ class wpkg::firefox3 {
 
   download {
     "firefox":
-      url => "http://download.mozilla.org/?product=firefox-3.5.3&os=win&lang=ca",
-      creates => "Firefox Setup 3.5.3.exe";
+      url => "http://download.mozilla.org/?product=firefox-3.5.4&os=win&lang=ca",
+      creates => "Firefox Setup 3.5.4.exe";
   }
 
 }
