9ff28fc64eb77c217501b739a7847e0e9802770f
Lluis Gili <lluis@ingent.net>
2009-11-23 14:34:45 +0100

    check for alias

--- puppet/modules/base/manifests/sshkeys.pp
+++ puppet/modules/base/manifests/sshkeys.pp
@@ -1,10 +1,16 @@
 class sshkeys {
 
+  if $alias_for_sshkey {
+    $alias = $alias_for_sshkey
+  } else {
+    $alias = ""
+  }
+
   # export ssh host key
-  # tags are given from initr, see http://projects.reductivelabs.com/issues/2825
+  # tags and alias are given from initr, see http://projects.reductivelabs.com/issues/2825
   @@sshkey { $fqdn:
     ensure => present,
-    alias => $alias_for_sshkey,
+    alias => $alias,
     key => $sshrsakey,
     type => "rsa",
     tag => $tags_for_sshkey,
