03894340867a6092d8e0010c5bf0a8d2d1cb54bc
Lluis Gili <lluis@ingent.net>
2009-11-23 18:13:47 +0100

    fix to stop reports: alias changed '' to ''

--- puppet/modules/base/manifests/sshkeys.pp
+++ puppet/modules/base/manifests/sshkeys.pp
@@ -2,18 +2,27 @@ class sshkeys {
 
   $array=[]
   $alias = $alias_for_sshkey ? {
-    $array => "",
+    $array => undef,
     default => $alias_for_sshkey,
   }
 
   # export ssh host key
   # tags and alias are given from initr, see http://projects.reductivelabs.com/issues/2825
-  @@sshkey { $fqdn:
-    ensure => present,
-    alias => $alias,
-    key => $sshrsakey,
-    type => "rsa",
-    tag => $tags_for_sshkey,
+  if $alias {
+    @@sshkey { $fqdn:
+      ensure => present,
+      alias => $alias,
+      key => $sshrsakey,
+      type => "rsa",
+      tag => $tags_for_sshkey,
+    }
+  } else {
+    @@sshkey { $fqdn:
+      ensure => present,
+      key => $sshrsakey,
+      type => "rsa",
+      tag => $tags_for_sshkey,
+    }
   }
 
 }
