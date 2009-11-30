9e10af17a6cdcded848cf1d0ad45a1373a206e08
Lluis Gili <lluis@ingent.net>
2009-11-30 13:19:01 +0100

    copy klass, closes #5

---
+++ puppet/modules/copy_klass/app/controllers/copy_klass_controller.rb
@@ -0,0 +1,30 @@
+class CopyKlassController < ApplicationController
+  unloadable
+
+  before_filter :find_copy_klass
+
+  def configure
+    @copiable_klasses = []
+    User.current.projects.collect {|p| p.nodes }.compact.flatten.each do |node|
+      next if node == @node
+      n = [node.fqdn, node.klasses.collect {|k| [k.name, k.id.to_s]}]
+      @copiable_klasses << n
+    end
+    if request.post?
+      params["copy_klass"] ||= {}
+      if @copy_klass.update_attributes(params["copy_klass"])
+        flash[:notice]='Configuration saved'
+        redirect_to :controller => 'klass', :action=>'list', :id => @node
+      else
+        render :action=>'configure'
+      end
+    end
+  end
+
+  def find_copy_klass
+    @copy_klass = Initr::CopyKlass.find params[:id]
+    @node = @copy_klass.node
+    @project = @node.project
+  end
+
+end
