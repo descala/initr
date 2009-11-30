bb0435b1ec90f89a18cba6574a64e6e9e6c4e507
Lluis Gili <lluis@ingent.net>
2009-11-30 18:11:13 +0100

    validates_uniqueness_of doesn't work with serialized attrs. Made custom validation

--- app/models/initr/klass.rb
+++ app/models/initr/klass.rb
@@ -91,6 +91,5 @@ class Initr::Klass < ActiveRecord::Base
     end
   end
 
-  
 end
 
--- puppet/modules/copy_klass/app/models/initr/copy_klass.rb
+++ puppet/modules/copy_klass/app/models/initr/copy_klass.rb
@@ -2,7 +2,13 @@ class Initr::CopyKlass < Initr::Klass
 
   unloadable
   validates_presence_of :copied_klass_id, :on => :update
-  validates_uniqueness_of :copied_klass_id, :scope => :node_id
+  validate :copied_klass_unique_for_node, :on => :update
+
+  def copied_klass_unique_for_node
+    node.klasses.all(:conditions=>["type='CopyKlass'"]).each do |ck|
+      errors.add_to_base("Klass already copied on this node") if ck.copied_klass_id == copied_klass_id
+    end
+  end
 
   # Allow more than one CopyKlass per node
   # see validates_uniqueness_of on Klass
