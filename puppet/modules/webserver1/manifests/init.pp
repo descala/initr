32701daa6f1eae5fb00ec1e1d9083488d33fad09
Lluis Gili <lluis@ingent.net>
2009-12-01 13:51:36 +0100

    fit webserver1::vsftpd class

--- puppet/modules/webserver1/manifests/init.pp
+++ puppet/modules/webserver1/manifests/init.pp
@@ -240,17 +240,19 @@ class webserver1::web_backups_server {
   }
 }
 
-class webserver1::ftp inherits ftp {
+class webserver1::ftp inherits ::ftp {
 
   # user shell should appear on /etc/shells to allow ftp login
   append_if_no_such_line { nologin_shell:
     file => "/etc/shells",
     line => "$nologin",
+    before => File[$vsftpd_conf_file],
   }
 
   file { $vsftpd_conf_file:
     content => template("webserver1/vsftpd.conf.erb"),
     require => Package["vsftpd"],
+    notify => Service["vsftpd"]
   }
 
 }
