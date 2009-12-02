cb24211563928edc12c34300989e80eec670d3b2
David Escala <descala@ingent.net>
2009-12-02 17:01:54 +0100

    Awstat confs require awstat package

--- puppet/modules/webserver1/manifests/init.pp
+++ puppet/modules/webserver1/manifests/init.pp
@@ -332,6 +332,7 @@ define webserver1::awstats::domain($user, $pass) {
   
   file {
     "/etc/awstats/awstats.$name.conf":
+      require => Package[awstats],
       mode => 644,
       content => template("webserver1/awstats.domain.conf.erb");
   }
