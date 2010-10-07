f0c913e749b78ff5e0eb0856c5b139f0e7e43723
Lluis Gili <lluis@ingent.net>
2010-10-07 14:26:47 +0200

    debian libstomp-ruby package is quite old, use rubygems

--- puppet/modules/base/manifests/stomp.pp
+++ puppet/modules/base/manifests/stomp.pp
@@ -1,20 +1,9 @@
 class stomp {
 
-  case $operatingsystem {
-    Debian: {
-      package {
-        "libstomp-ruby":
-          ensure => installed,
-          alias => "stomp";
-      }
-    }
-    default: {
-      include rubygems
-      gem_package {
-        "stomp":
-          ensure => "1.1.6";
-      }
-    }
+  include rubygems
+  gem_package {
+    "stomp":
+      ensure => "1.1.6";
   }
 
 }
