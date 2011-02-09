446ff2ce97db555326160a293a31ee6ba6de37cb
Lluis Gili <lluis@ingent.net>
2011-02-09 13:21:15 +0100

    rm bad located facts

--- puppet/facts/ipaddress_internet.rb
+++
@@ -1,8 +0,0 @@
-require 'facter'
-require 'open-uri'
-
-Facter.add("ipaddress_internet") do
-  setcode do
-    open("http://checkip.dyndns.org/").read.match(/Current IP Address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/).captures.first rescue nil
-  end
-end
--- puppet/facts/puppet_classes.rb
+++
@@ -1,9 +0,0 @@
-require 'facter'
-
-# Puppet classes currently applied
-Facter.add("puppet_classes") do
-  setcode do
-    path = "/var/lib/puppet/state/classes.txt"
-    open(path).read.split("\n").sort.join(" ") if File.file?(path)
-  end
-end
