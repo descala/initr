ef2d5aaa8ccf58cc2a42cc33f7627783d9815c4e
Lluis Gili <lluis@ingent.net>
2011-02-09 13:49:44 +0100

    remove plugins dir

--- puppet/modules/base/plugins/facter/puppetd_pid.rb
+++
@@ -1,9 +0,0 @@
-require 'facter'
-
-# PID del proces puppetd
-
-Facter.add("puppetd_pid") do
-	setcode do
-			 %x{/sbin/pidof -x puppetd}.chomp
-	end
-end
--- puppet/modules/base/plugins/facter/rubygems.rb
+++
@@ -1,8 +0,0 @@
-require 'facter'
-
-Facter.add("rubygems_path") do
-  setcode do
-    %x{gem environment gempath}.chomp
-  end
-end
-
