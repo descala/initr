7f3ad813598080ff7b834eefb9e17fbf0484cda1
Lluis Gili <lluis@ingent.net>
2011-04-20 10:18:06 +0200

    check domain and ttl, closes #2719

--- puppet/modules/bind/app/models/initr/bind_zone.rb
+++ puppet/modules/bind/app/models/initr/bind_zone.rb
@@ -3,6 +3,9 @@ class Initr::BindZone < ActiveRecord::Base
   belongs_to :bind, :class_name => "Initr::Bind"
   validates_presence_of :domain, :ttl
   validates_uniqueness_of :domain, :scope => 'bind_id'
+  validates_numericality_of :ttl
+  validates_format_of :domain, :with => /^[\w\d]+([\-\.]{1}[\w\d]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i
+  validates_format_of :domain, :with => /^[^_]+$/i
 
   def initialize(attributes=nil)
     super
