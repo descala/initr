a0b5c82a439b36ad271f0f5931d95b2992e25217
Lluis Gili <lluis@ingent.net>
2011-06-02 11:39:19 +0200

    firefox 4

--- puppet/modules/wpkg/manifests/firefox.pp
+++ puppet/modules/wpkg/manifests/firefox.pp
@@ -2,18 +2,33 @@
 
 # Class name must match a package id in an XML in files/packages
 
-class wpkg::firefox3 {
+class wpkg::firefox {
 
   file {
     "$wpkg_base/software/firefox":
       ensure => directory;
   }
 
+}
+
+class wpkg::firefox3 inherits firefox {
+
   download {
     "firefox":
       to => "firefox",
       url => "http://download.mozilla.org/?product=firefox-3.6.8&os=win&lang=ca",
       creates => "Firefox Setup 3.6.8.exe";
+  }
+
+}
+
+class wpkg::firefox4 inherits firefox {
+
+  download {
+    "firefox4":
+      to => "firefox",
+      url => "http://download.mozilla.org/?product=firefox-4.0.1&os=win&lang=ca",
+      creates => "Firefox Setup 4.0.1.exe";
   }
 
 }
