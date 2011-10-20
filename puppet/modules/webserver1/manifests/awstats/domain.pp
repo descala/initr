80a79849626cf2410043dc60a5e292bcd99d1d4a
Lluis Gili <lluis@ingent.net>
2011-10-20 12:00:20 +0200

    fix for centos 4.9

--- puppet/modules/webserver1/manifests/awstats/domain.pp
+++ puppet/modules/webserver1/manifests/awstats/domain.pp
@@ -1,16 +1,19 @@
 define webserver1::awstats::domain($user, $pass) {
   
-  file {
-    "/etc/awstats/awstats.$name.conf":
-      require => Package[awstats],
-      mode => 644,
-      content => template("webserver1/awstats.domain.conf.erb");
-  }
+  if ( $operatingsystem == "CentOS" ) and ( $operatingsystemrelease == "4.9" ) {
+  } else {
+    file {
+      "/etc/awstats/awstats.$name.conf":
+        require => Package[awstats],
+        mode => 644,
+        content => template("webserver1/awstats.domain.conf.erb");
+    }
 
-  exec { "htpasswd for $user":
-    command => "/usr/bin/htpasswd -b /etc/awstats/users $user $pass",
-    user => root,
-    unless => "grep \"^$user:\" /etc/awstats/users";
+    exec { "htpasswd for $user":
+      command => "/usr/bin/htpasswd -b /etc/awstats/users $user $pass",
+      user => root,
+      unless => "grep \"^$user:\" /etc/awstats/users";
+    }
   }
 
 }
