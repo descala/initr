10a620faffafe551b7e87a989cd64e25558feaaf
Lluis Gili <lluis@ingent.net>
2011-10-20 11:47:11 +0200

    apache::passenger for now only works for Debian

--- puppet/modules/common/manifests/apache/passenger.pp
+++ puppet/modules/common/manifests/apache/passenger.pp
@@ -1,8 +1,12 @@
 class common::apache::passenger {
 
-  # this class assumes that you installed passenger and rails
-  # from gems or your distro package manager
-  common::apache::enmod { ["passenger.load","passenger.conf"]: }
+  if $operatingsystem == "Debian" {
+    # this class assumes that you installed passenger and rails
+    # from gems or your distro package manager
+    common::apache::enmod { ["passenger.load","passenger.conf"]: }
+  } else {
+    #TODO
+  }
 
 }
 
