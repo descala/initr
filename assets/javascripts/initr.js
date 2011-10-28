f1bf54b6d3b1f85dcf6f6a49b281b58fd8798dc4
Lluís <lluis@ingent.net>
2011-10-29 00:33:14 +0200

    help buttons

---
+++ assets/javascripts/initr.js
@@ -0,0 +1,15 @@
+/* Add function to window.onload */
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
+
