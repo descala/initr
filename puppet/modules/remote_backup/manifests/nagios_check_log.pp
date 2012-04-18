0540818c9de431a7d207032a1165f5319cf31932
Lluis Gili <lluis@ingent.net>
2012-04-18 11:02:40 +0200

    remove duplicated 'is_ok'

--- puppet/modules/remote_backup/manifests/nagios_check_log.pp
+++ puppet/modules/remote_backup/manifests/nagios_check_log.pp
@@ -1,6 +1,6 @@
 define remote_backup::nagios_check_log() {
 
-  nagios::check { "${name}_is_ok":
+  nagios::check { $name:
     command => "check_log -F /var/log/backupninja.log -O /var/log/old_backupninja.log -q Warning:\|Error:\|Fatal:\|Halt:",
     minute => 0,
     hour => 7,
