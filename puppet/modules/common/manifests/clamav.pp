0dcedd2df4caa305391aaea39ed8f5d7c84c020a
Lluis Gili <lluis@ingent.net>
2012-08-01 14:35:44 +0200

    install decompressors for clamav

--- puppet/modules/common/manifests/clamav.pp
+++ puppet/modules/common/manifests/clamav.pp
@@ -1,5 +1,7 @@
 class common::clamav {
 
+  include common::decompressors
+
   case $operatingsystem {
     "Debian": { include common::clamav::debian }
     "CentOS": { include common::clamav::centos }
---
+++ puppet/modules/common/manifests/decompressors.pp
@@ -0,0 +1,16 @@
+class common::decompressors {
+
+  case $operatingsystem {
+    "Debian": {
+      package {
+        ["arj","bzip2","cabextract","cpio","file","gzip","lha",
+        "nomarch","pax","rar","unrar","unzip","zip","zoo"]:
+          ensure => installed;
+      }
+    }
+    default: {
+      #todo
+    }
+  }
+
+}
