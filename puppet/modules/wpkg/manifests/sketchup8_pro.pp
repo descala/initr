6cf56ee1b6e89a3623d43cf41f40f4e46a8834c0
David Escala <descala@ingent.net>
2013-04-24 20:04:10 +0200

    New Google SkechtUp version

--- puppet/modules/wpkg/manifests/sketchup8_pro.pp
+++ puppet/modules/wpkg/manifests/sketchup8_pro.pp
@@ -1,7 +1,7 @@
 class wpkg::sketchup8_pro inherits wpkg::sketchup8 {
 
   Download["sketchup8"] {
-      url => "http://dl.google.com/sketchup/GoogleSketchUpProWES.exe",
+      url => "http://dl.trimble.com/sketchup/SketchUpProWES.exe",
   }
 
 }
