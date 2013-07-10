5ddfe8bef5f542397d54fde8edd82c29ff3cb6bd
Lluis Gili <lluis@ingent.net>
2013-07-10 10:01:46 +0200

    fix typo

--- puppet/modules/common/manifests/dovecot.pp
+++ puppet/modules/common/manifests/dovecot.pp
@@ -3,9 +3,9 @@ class common::dovecot($db_name,$db_user,$db_passwd,$db_passwd_encrypt,$mail_loca
   case $operatingsystem {
     "Debian": {
       case $lsbmajdistrelease {
-        "6": { include common::dovecot::debian6 },
+        "6": { include common::dovecot::debian6 }
         default: { include common::dovecot::debian7 }
-      },
+      }
     "CentOS": { include common::dovecot::centos }
   }
 
