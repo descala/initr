aed1160f694f1f133a2e7baa1c86fbfaabf86db6
Lluis Gili <lluis@ingent.net>
2013-10-28 11:44:07 +0100

    use curlmyip.com instead checkip.dyndns.org. Rescue and warn exceptions and try 5 times to get the IP

--- puppet/modules/base/lib/facter/ipaddress_internet.rb
+++ puppet/modules/base/lib/facter/ipaddress_internet.rb
@@ -3,10 +3,18 @@ require 'open-uri'
 
 Facter.add("ipaddress_internet") do
   setcode do
-    begin
-      open("http://checkip.dyndns.org/").read.match(/Current IP Address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/).captures.first
-    rescue Exception => e
-      e.message
+    i=0
+    ip=''
+    re=/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/
+    while i<5 and ip.empty?
+      begin
+        ip=open("http://curlmyip.com/").read.match(re).captures.first
+      rescue Exception => e
+        Facter.warnonce "Failed to get ip from curlmyip.com: #{e.message}"
+      ensure
+        i += 1
+      end
     end
+    ip
   end
 end
