5b86610feaf48ef7a850b8dfc4b7c5b70969c6b0
Lluis Gili <lluis@ingent.net>
2013-10-28 11:03:09 +0100

    show exception in fact, if any

--- puppet/modules/base/lib/facter/ipaddress_internet.rb
+++ puppet/modules/base/lib/facter/ipaddress_internet.rb
@@ -3,6 +3,10 @@ require 'open-uri'
 
 Facter.add("ipaddress_internet") do
   setcode do
-    open("http://checkip.dyndns.org/").read.match(/Current IP Address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/).captures.first rescue nil
+    begin
+      open("http://checkip.dyndns.org/").read.match(/Current IP Address: ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/).captures.first
+    rescue Exception => e
+      e.message
+    end
   end
 end
