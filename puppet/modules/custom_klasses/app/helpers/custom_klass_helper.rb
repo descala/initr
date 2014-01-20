55ef4cd13bfb0cd2d58f3a60967a5355ca51f953
David Escala <descala@ingent.net>
2014-01-20 15:20:11 +0100

    custom klass review

--- puppet/modules/custom_klasses/app/helpers/custom_klass_helper.rb
+++ puppet/modules/custom_klasses/app/helpers/custom_klass_helper.rb
@@ -1,9 +1,10 @@
 module CustomKlassHelper
 
+  # TODO redmine2
   def add_ckc_link(name)
-    link_to_function name do |page|
-      page.insert_html :bottom, :custom_klass_confs, :partial => 'custom_klass_conf' , :object => Initr::CustomKlassConf.new
-    end
+#    link_to_function name do |page|
+#      page.insert_html :bottom, :custom_klass_confs, :partial => 'custom_klass_conf' , :object => Initr::CustomKlassConf.new
+#    end
   end
   
 end
