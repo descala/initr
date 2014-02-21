2346ef207a7494b2c902e9a902361265b67351fd
David Escala <descala@ingent.net>
2014-02-21 13:52:01 +0100

    Removes duplicate indexes

--- db/migrate/20110725134741_add_indexes_to_klasses.rb
+++ db/migrate/20110725134741_add_indexes_to_klasses.rb
@@ -2,10 +2,12 @@ class AddIndexesToKlasses < ActiveRecord::Migration
   def self.up
     add_index :klasses, :node_id
     add_index :klasses, :type
+    add_index :klasses, :active
   end
   
   def self.down
     remove_index :klasses, :node_id
     remove_index :klasses, :type
+    remove_index :klasses, :active
   end
 end
