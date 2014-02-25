5289ab39c2a782704777f64b57e60e52e3290847
Lluis Gili <lluis@ingent.net>
2014-02-25 15:09:35 +0100

    change exe name for sketchup2013

--- puppet/modules/wpkg/manifests/sketchup8_pro.pp
+++ puppet/modules/wpkg/manifests/sketchup8_pro.pp
@@ -1,9 +1,10 @@
 class wpkg::sketchup8_pro inherits wpkg::sketchup8 {
 
-  Download["sketchup8"] {
-      url => "http://www.iscarnet.com/descargas/SketchUp+Pro+2013+Windows",
+  Download['sketchup8'] {
+      url     => 'http://www.iscarnet.com/descargas/SketchUp+Pro+2013+Windows',
+      creates => 'sketchupprowes.exe'
   }
-  Exec["sketchup8_extract"] {
+  Exec['sketchup8_extract'] {
     creates => "$wpkg_base/software/sketchup/SketchUp2013.msi"
   }
 
