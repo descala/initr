dfe5e25d071c1f8ed202e16a3538bdab94ee9724
Lluis Gili <lluis@ingent.net>
2014-05-15 15:10:55 +0200

    create empty /etc/pam.d/dovecot to avoid errors

--- puppet/modules/common/manifests/dovecot/debian7.pp
+++ puppet/modules/common/manifests/dovecot/debian7.pp
@@ -1,11 +1,19 @@
 class common::dovecot::debian7 inherits common::dovecot::debian {
 
-  package { "dovecot-mysql":
+  package { 'dovecot-mysql':
     ensure => installed,
   }
 
   Service[dovecot] {
-    require => [Package["dovecot-imapd"],Package["dovecot-pop3d"],Package["dovecot-mysql"]]
+    require => [Package['dovecot-imapd'],Package['dovecot-pop3d'],Package['dovecot-mysql']]
+  }
+
+  # prevent error on every login:
+  # pam_authenticate() failed: Authentication failure (/etc/pam.d/dovecot missing?)
+  file {
+    '/etc/pam.d/dovecot':
+      content => '',
+      replace => false;
   }
 
 }
