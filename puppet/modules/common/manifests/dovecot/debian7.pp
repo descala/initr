2d60da98379979f368a1a39709dd9f3cb07f9171
Lluís <lluis@ingent.net>
2014-05-15 17:14:37 +0200

    make dovenull user belong to dovenull group

--- puppet/modules/common/manifests/dovecot/debian7.pp
+++ puppet/modules/common/manifests/dovecot/debian7.pp
@@ -1,11 +1,17 @@
 class common::dovecot::debian7 inherits common::dovecot::debian {
 
-  package { "dovecot-mysql":
+  package { 'dovecot-mysql':
     ensure => installed,
   }
 
   Service[dovecot] {
-    require => [Package["dovecot-imapd"],Package["dovecot-pop3d"],Package["dovecot-mysql"]]
+    require => [Package['dovecot-imapd'],Package['dovecot-pop3d'],Package['dovecot-mysql']]
+  }
+
+  exec {
+    # see /usr/share/doc/dovecot-core/README.Debian.gz
+    'addgroup --system --group dovenull ; usermod -g dovenull dovenull':
+      onlyif => '/usr/bin/groups dovenull | /bin/grep -q nogroup';
   }
 
 }
