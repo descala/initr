8342e6a8376eb80eb5da1378ba7e69a5a0010871
Lluis Gili <lluis@ingent.net>
2014-05-16 11:21:08 +0200

    puppet syntax

--- puppet/modules/common/manifests/amavis/debian.pp
+++ puppet/modules/common/manifests/amavis/debian.pp
@@ -1,28 +1,28 @@
 class common::amavis::debian inherits common::amavis::common {
 
   service {
-    "amavis":
-      enable => true,
-      ensure => running,
+    'amavis':
+      ensure     => running,
+      enable     => true,
       hasrestart => true,
-      hasstatus => true,
-      require => Package["amavisd-new"];
+      hasstatus  => true,
+      require    => Package['amavisd-new'];
   }
 
   cron {
-    "purge quarantine":
-      command => "find /var/lib/amavis/virusmails/ -type f -ctime +30 -delete",
-      user => root,
-      hour => 6,
-      minute => 15;
+    'purge quarantine':
+      command => 'find /var/lib/amavis/virusmails/ -type f -ctime +30 -delete',
+      user    => root,
+      hour    => 6,
+      minute  => 15;
   }
 
   file {
-    "/etc/amavis/conf.d/15-content_filter_mode":
-      source => ["puppet:///specific/amavis-15-content_filter_mode","puppet:///modules/common/amavis/15-content_filter_mode"],
-      mode => 644,
-      require => Package["amavisd-new"],
-      notify => Service["amavis"];
+    '/etc/amavis/conf.d/15-content_filter_mode':
+      source  => ['puppet:///specific/amavis-15-content_filter_mode','puppet:///modules/common/amavis/15-content_filter_mode'],
+      mode    => '0644',
+      require => Package['amavisd-new'],
+      notify  => Service['amavis'];
   }
 
 }
