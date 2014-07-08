21eddd9f004d92be3d74999c2c923bed359afe08
David Escala <descala@ingent.net>
2014-07-08 10:15:44 +0200

    do not mess with delayed_jobs

--- db/migrate/20090505162000_setup.rb
+++ db/migrate/20090505162000_setup.rb
@@ -45,19 +45,6 @@ class Setup < ActiveRecord::Migration
       t.timestamps
     end
 
-    # Delayed_job tables
-    create_table :delayed_jobs do |table|
-      table.integer  :priority, :default => 0
-      table.integer  :attempts, :default => 0
-      table.text     :handler
-      table.string   :last_error
-      table.datetime :run_at
-      table.datetime :locked_at
-      table.string   :locked_by
-      table.datetime :failed_at
-      table.timestamps
-    end
-
   end
 
   def self.down
