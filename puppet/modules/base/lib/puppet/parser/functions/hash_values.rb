53aae242f2bbfca90329af49d83e4e60d7f74317
Lluis Gili <lluis@ingent.net>
2014-09-17 11:43:29 +0200

    fix hash_values function

--- puppet/modules/base/lib/puppet/parser/functions/hash_values.rb
+++ puppet/modules/base/lib/puppet/parser/functions/hash_values.rb
@@ -1,6 +1,6 @@
 # Given a Hash, return its values as an array
 module Puppet::Parser::Functions
-  newfunction(:hash_values, :type=>:rvalue) do |args|
+  newfunction(:hash_values, :type=>:rvalue) do |arguments|
     raise(Puppet::ParseError, "hash_values(): Wrong number of arguments " +
           "given (#{arguments.size} for 1)") if arguments.size < 1
     hash = arguments[0]
