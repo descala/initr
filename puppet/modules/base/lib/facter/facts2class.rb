58520fc8b4af9708801e1ff4d0d976d821013f53
Lluís <lluis@ingent.net>
2014-12-04 08:37:02 +0100

    new facter versions don't have accessors for facts

--- puppet/modules/base/lib/facter/facts2class.rb
+++ puppet/modules/base/lib/facter/facts2class.rb
@@ -3,13 +3,13 @@ require 'facter'
 # fqdn without dots, to write an specific class for each node
 Facter.add("fqdnclass") do
   setcode do
-    Facter.fqdn.split('.').join('_')
+    Facter["fqdn"].value.split('.').join('_')
   end
 end
 
 # lsbdistrelease without dots, to write an specific class for each node
 Facter.add("lsbdistrelease_class") do
   setcode do
-    Facter.lsbdistrelease.split('.').join('_') unless Facter.lsbdistrelease.nil?
+    Facter["lsbdistrelease"].value.split('.').join('_') unless Facter["lsbdistrelease"].nil?
   end
 end
