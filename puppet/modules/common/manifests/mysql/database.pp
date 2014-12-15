8c9fd2ff69cbc832065c021c0d8519a60c5858ff
Lluís <lluis@ingent.net>
2014-12-15 21:32:12 +0100

    syntax

--- puppet/modules/common/manifests/mysql/database.pp
+++ puppet/modules/common/manifests/mysql/database.pp
@@ -1,32 +1,32 @@
 define common::mysql::database($ensure, $owner, $passwd) {
 
-  $dbcr = "CREATE DATABASE IF NOT EXISTS $name;"
-  $priv = "GRANT ALL PRIVILEGES ON $name.* TO '$owner'@localhost IDENTIFIED BY '$passwd';"
+  $dbcr = "CREATE DATABASE IF NOT EXISTS ${name};"
+  $priv = "GRANT ALL PRIVILEGES ON ${name}.* TO '${owner}'@localhost IDENTIFIED BY '${passwd}';"
 
   case $ensure {
     present: {
       exec {
-        "Create MySQL DB: $name":
-          command => "mysql -e \"$dbcr\"",
-          user => "root",
-          unless => "/usr/bin/mysql $name -e \";\"",
-          require => Service[$mysqld];
-        "MySQL Privileges for $owner to DB $name":
-          command => "mysql -e \"$priv\"",
-          user => "root",
-          unless => "/usr/bin/mysqlshow --user=$owner --password=$passwd $name",
-          require => Exec["Create MySQL DB: $name"];
+        "Create MySQL DB: ${name}":
+          command => "mysql -e \"${dbcr}\"",
+          user    => root,
+          unless  => "/usr/bin/mysql ${name} -e \";\"",
+          require => Service[$::mysqld];
+        "MySQL Privileges for ${owner} to DB ${name}":
+          command => "mysql -e \"${priv}\"",
+          user    => root,
+          unless  => "/usr/bin/mysqlshow --user=${owner} --password=${passwd} ${name}",
+          require => Exec["Create MySQL DB: ${name}"];
       }
     }
     absent: {
-      exec { "Drop MySQL DB: $name":
-        command => "mysql -e \"DROP DATABASE $name;\"",
-        user => "root",
-        onlyif => "/usr/bin/mysqlshow $name",
+      exec { "Drop MySQL DB: ${name}":
+        command => "mysql -e \"DROP DATABASE ${name};\"",
+        user    => root,
+        onlyif  => "/usr/bin/mysqlshow ${name}",
       }
     }
     default: {
-      fail "Invalid 'ensure' value '$ensure' for mysql::database"
+      fail "Invalid 'ensure' value '${ensure}' for mysql::database"
     }
   }
 }
