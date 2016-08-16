652e70538d56721d5f41c9b4f4647776dc54c779
Lluís Gili <lluis@ingent.net>
2016-08-16 14:00:40 +0200

    nagios checks from mailserver1

--- puppet/modules/mailserver/manifests/nagios.pp
+++ puppet/modules/mailserver/manifests/nagios.pp
@@ -1,3 +1,21 @@
-class mailserver::nagios inherits mailserver1::nagios {
-  # Put common nagios checks on mailserver1
+# nagios checks for mail
+class mailserver::nagios {
+  nagios::check { 'imap':
+    command => "check_imap -H localhost -e 'OK'",
+  }
+  nagios::check { 'pop':
+    command => "check_pop -H localhost -e 'OK'",
+  }
+  if $::operatingsystem == 'CentOS' and $::lsbmajdistrelease == '5' {
+    nagios::check { 'smtp_ssl':
+      command => 'check_smtp -H localhost -S -D 30',
+    }
+  } else {
+    nagios::check { 'smtp_ssl':
+      command => 'check_smtp -H localhost -S -D 30,15',
+    }
+  }
+
+  # shared checks for smtp and mail queue
+  include common::postfix::nagios
 }
