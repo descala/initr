a6ffcbd400d5318498f58fbf39bb2daf41f5993b
Lluís Gili <lluis@ingent.net>
2016-08-19 12:01:20 +0200

    add --defaults-file=/root/.my.cnf to mysql cmd on Debian 8

--- puppet/modules/common/manifests/mysql/database.pp
+++ puppet/modules/common/manifests/mysql/database.pp
@@ -3,16 +3,22 @@ define common::mysql::database($ensure, $owner, $passwd) {
   $dbcr = "CREATE DATABASE IF NOT EXISTS ${name};"
   $priv = "GRANT ALL PRIVILEGES ON ${name}.* TO '${owner}'@localhost IDENTIFIED BY '${passwd}';"
 
+  if $::operatingsystem == 'Debian' and $::lsbmajdistrelease == '8' {
+    $cmd='mysql --defaults-file=/root/.my.cnf'
+  } else {
+    $cmd='mysql'
+  }
+
   case $ensure {
     present: {
       exec {
         "Create MySQL DB: ${name}":
-          command => "mysql -e \"${dbcr}\"",
+          command => "${cmd} -e \"${dbcr}\"",
           user    => root,
           unless  => "/usr/bin/mysql ${name} -e \";\"",
           require => Service[$::mysqld];
         "MySQL Privileges for ${owner} to DB ${name}":
-          command => "mysql -e \"${priv}\"",
+          command => "${cmd} -e \"${priv}\"",
           user    => root,
           unless  => "/usr/bin/mysqlshow --user=${owner} --password=${passwd} ${name}",
           require => Exec["Create MySQL DB: ${name}"];
@@ -20,7 +26,7 @@ define common::mysql::database($ensure, $owner, $passwd) {
     }
     absent: {
       exec { "Drop MySQL DB: ${name}":
-        command => "mysql -e \"DROP DATABASE ${name};\"",
+        command => "${cmd} -e \"DROP DATABASE ${name};\"",
         user    => root,
         onlyif  => "/usr/bin/mysqlshow ${name}",
       }
