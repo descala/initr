ab1c18bef9ca3bfdf961c0976303845b221812d8
Lluís Gili <lluis@ingent.net>
2016-08-19 12:06:18 +0200

    on debian 8 install libapache2-mpm-itk instead of libapache2-mod-suphp

--- puppet/modules/common/manifests/suphp.pp
+++ puppet/modules/common/manifests/suphp.pp
@@ -1,27 +1,31 @@
 class common::suphp {
-  $suphp_package = $operatingsystem ? {
-    /Debian|Ubuntu/ => "libapache2-mod-suphp",
-    default => "mod_suphp"
+  $suphp_package = $::operatingsystem ? {
+    'Debian' => $::lsbmajdistrelease ? {
+      '8'     => 'libapache2-mpm-itk',
+      default => 'libapache2-mod-suphp'
+    }
+    'Ubuntu' => 'libapache2-mod-suphp',
+    default  => 'mod_suphp'
   }
   package {
     $suphp_package:
       ensure => installed,
-      notify => Exec["apache reload"];
+      notify => Exec['apache reload'];
   }
   # Debian provided suphp.conf works
-  if ( $operatingsystem == 'Debian' ) or ( $operatingsystem == 'Ubuntu' ) {
-    if ($operatingsystem == 'Debian' and $lsbmajdistrelease == '8') {
+  if ( $::operatingsystem == 'Debian' ) or ( $::operatingsystem == 'Ubuntu' ) {
+    if ($::operatingsystem == 'Debian' and $::lsbmajdistrelease == '8') {
     common::apache::enmod { 'mpm_itk.load': }
     } else {
-    common::apache::enmod { "suphp.load": }
-    common::apache::enmod { "suphp.conf": }
+    common::apache::enmod { 'suphp.load': }
+    common::apache::enmod { 'suphp.conf': }
     }
   } else {
     file {
-      "/etc/suphp.conf":
-        source => "puppet:///modules/common/suphp/suphp-redhat.conf";
-      "/etc/logrotate.d/suphp":
-        source => "puppet:///modules/common/suphp/logrotate.conf";
+      '/etc/suphp.conf':
+        source => 'puppet:///modules/common/suphp/suphp-redhat.conf';
+      '/etc/logrotate.d/suphp':
+        source => 'puppet:///modules/common/suphp/logrotate.conf';
     }
   }
 }
