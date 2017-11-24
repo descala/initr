4b41d87218673928f99c6abd823a6840b41153a1
David Escala <descala@ingent.net>
2017-11-24 12:51:54 +0100

    Do not manage php5-mysql
    TODO: In Debian > 8 should be php5-mysqlnd

--- puppet/modules/common/manifests/php.pp
+++ puppet/modules/common/manifests/php.pp
@@ -1,12 +1,29 @@
 class common::php {
   $packages = $operatingsystem ? {
-    Debian => ["php5", "php5-mysql"],
-    default => ["php","php-eaccelerator","php-mysql"]
+    Debian => ["php5"],
+    default => ["php","php-eaccelerator"]
   }
   package {
     $packages:
       ensure => installed,
       notify => Exec["apache reload"];
   }
-}
 
+  # define php_packages($packages) {
+  #   package {
+  #     $packages:
+  #       ensure => installed,
+  #       notify => Exec["apache reload"];
+  #   }
+  # }
+
+  # case $operatingsystem {
+  #   "Debian": {
+  #     case $lsbmajdistrelease {
+  #       "8", "9": { php_packages(["php5", "php5-mysqlnd"]) }
+  #       default:  { php_packages(["php5", "php5-mysql"]) }
+  #     }
+  #   }
+  #   default: { php_packages(["php","php-eaccelerator","php-mysql"]) }
+  # }
+}
