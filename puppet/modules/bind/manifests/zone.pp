a8976693e2536e7ba4fe0d20d9b3355626eadec7
Lluís Gili <lluis@ingent.net>
2018-02-03 18:05:37 +0100

    update bind module for puppet4

--- puppet/modules/bind/manifests/zone.pp
+++ puppet/modules/bind/manifests/zone.pp
@@ -4,7 +4,7 @@ define bind::zone($zone,$ttl,$serial) {
     "$bind::var_dir/puppet_zones/$name.zone":
       owner => $bind::binduser,
       group => $bind::binduser,
-      mode => 640,
+      mode => '0640',
       content => template("bind/zone.erb"),
       require => [Package[$bind::bind],File["$bind::var_dir/puppet_zones"]],
       notify => Service[$bind::bind];
