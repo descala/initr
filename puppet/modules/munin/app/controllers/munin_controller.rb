61fc264a3e94efa94f9b63785a7e557b33445841
David Escala <descala@ingent.net>
2018-03-31 20:37:04 +0200

    fix munin server for puppet 4

--- puppet/modules/munin/app/controllers/munin_controller.rb
+++ puppet/modules/munin/app/controllers/munin_controller.rb
@@ -11,7 +11,7 @@ class MuninController < InitrController
     @munin_server = @node.klasses.find_by_type("Initr::Munin").server.node rescue nil
     render :text => "no server defined" if @munin_server.nil?
     @munin_klass = Initr::Munin.for_node(@node)
-    @graphs = @node.puppet_fact("munin_graphs","").split(",").sort
+    @graphs = @node.fact("munin_graphs","").split(",").sort
     if params['period'].nil? or params['period'] == "day" or params['period'] == "week"
       @period=["day","week"]
     else
@@ -43,7 +43,7 @@ class MuninController < InitrController
   def munins_for_current_user
     user_projects = User.current.projects
     Initr::Munin.all.collect { |m|
-      mgraphs = m.node.puppet_fact('munin_graphs','').split(',')
+      mgraphs = m.node.fact('munin_graphs','').split(',')
       m if user_projects.include?(m.node.project) and mgraphs.include?(@name)
     }.compact.sort {|a,b| a.node.fqdn <=> b.node.fqdn }
   end
