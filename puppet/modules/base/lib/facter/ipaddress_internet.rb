6fcc3c964b6adae283d5b65d0191f73b66b24fbb
David Escala <descala@ingent.net>
2019-03-28 12:03:35 +0100

    Force use IPv4 in ipaddress_internet fact

--- puppet/modules/base/lib/facter/ipaddress_internet.rb
+++ puppet/modules/base/lib/facter/ipaddress_internet.rb
@@ -1,5 +1,5 @@
 Facter.add('ipaddress_internet') do
   setcode do
-    Facter::Core::Execution.execute('dig +short myip.opendns.com @resolver1.opendns.com')
+    Facter::Core::Execution.execute('dig -4 +short myip.opendns.com @resolver1.opendns.com')
   end
 end
