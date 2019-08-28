600449c3a99903c848d6c014eec09652096bcbfa
Lluís Gili <lluis@ingent.net>
2019-08-28 12:42:52 +0200

    add buster to package manager

--- puppet/modules/package_manager/manifests/debian.pp
+++ puppet/modules/package_manager/manifests/debian.pp
@@ -8,13 +8,15 @@ class package_manager::debian {
     'wheezy':  { include package_manager::debian::wheezy  }
     'jessie':  { include package_manager::debian::jessie  }
     'stretch': { include package_manager::debian::stretch }
+    'buster':  { include package_manager::debian::buster  }
     'n/a': {
       case $::lsbmajdistrelease {
-        '5': { include package_manager::debian::lenny   }
-        '6': { include package_manager::debian::squeeze }
-        '7': { include package_manager::debian::wheezy  }
-        '8': { include package_manager::debian::jessie  }
-        '9': { include package_manager::debian::stretch }
+        '5':  { include package_manager::debian::lenny   }
+        '6':  { include package_manager::debian::squeeze }
+        '7':  { include package_manager::debian::wheezy  }
+        '8':  { include package_manager::debian::jessie  }
+        '9':  { include package_manager::debian::stretch }
+        '10': { include package_manager::debian::buster  }
         default: {}
       }
     }
---
+++ puppet/modules/package_manager/manifests/debian/buster.pp
@@ -0,0 +1,23 @@
+# on debian buster use unattended-upgrades instead of cron-apt
+class package_manager::debian::buster {
+
+  include package_manager::debian::sources
+
+  package {
+    'cron-apt':
+      ensure => purged;
+    'unattended-upgrades':
+      ensure => installed;
+  }
+  file {
+    '/etc/cron-apt/action.d/5-install':
+      ensure => absent;
+    '/etc/apt/preferences':
+      content => template('package_manager/preferences_buster.erb'),
+      notify  => Exec['apt-get update'];
+    '/etc/apt/apt.conf.d/50unattended-upgrades':
+      source  => 'puppet:///modules/package_manager/50unattended-upgrades',
+      require => Package['unattended-upgrades'];
+  }
+
+}
