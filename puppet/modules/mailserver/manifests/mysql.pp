dd333d3c875b2e7a1da237563b26fdbc0699c271
Lluís Gili <lluis@ingent.net>
2019-12-02 18:48:36 +0100

    update for puppet 5

--- puppet/modules/mailserver/manifests/mysql.pp
+++ puppet/modules/mailserver/manifests/mysql.pp
@@ -1,24 +1,25 @@
+# mailserver mysql
 class mailserver::mysql {
   include common::mysql
   cron {
     'maildb-bak':
-      command => 'mysqldump $::db_name > /root/maildb-bak.sql',
+      command => "mysqldump ${::db_name} > /root/maildb-bak.sql",
       user    => root,
       hour    => 3,
       minute  => 0,
       require => Service[$::mysqld];
     'maildb-scp':
-      command => '/usr/bin/scp /root/maildb-bak.sql backup_$::hostname@$::bak_host:/var/arxiver/backups/backup_$::hostname/backup_mail_$::hostname/',
+      command => "/usr/bin/scp /root/maildb-bak.sql backup_${::hostname}@${::bak_host}:/var/arxiver/backups/backup_${::hostname}/backup_mail_${::hostname}/",
       user    => root,
       hour    => 3,
       minute  => 15,
       require => Cron['maildb-bak'];
   }
   common::mysql::database { $::db_name:
-    ensure    => present,
-    owner     => $::db_user,
-    passwd    => $::db_passwd,
-    require   => Package['postfixadmin'];
+    ensure  => present,
+    owner   => $::db_user,
+    passwd  => $::db_passwd,
+    require => Package['postfixadmin'];
   }
   package {
     'postfix-mysql':
