42da740483fe0f3ebf5844174977edb029174382
Lluís Gili <lluis@ingent.net>
2020-05-07 12:04:14 +0200

    Revert "no facilitar llistat dominis a tmp"
    This reverts commit 2382b12c9932fa23eda66a64265c0d6ea3235e07.

--- puppet/modules/common/files/amavis/amavis_update_local_domains.sh
+++ puppet/modules/common/files/amavis/amavis_update_local_domains.sh
@@ -3,7 +3,6 @@
 /usr/bin/mysql postfix --skip-column-names -s -e "select domain from domain where domain like '%.%';" > /tmp/domains
 
 if [ -e /tmp/domains ] ; then
-    chmod 600 /tmp/domains
     # exit if nothing changed
     cmp -s /tmp/domains /etc/amavis/local_domains && exit
     mv /tmp/domains /etc/amavis/local_domains && /usr/sbin/service amavis restart > /dev/null
