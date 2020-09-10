cb14a96421e3c8bd28a4bbd35c2961504a4c4338
Lluís Gili <lluis@ingent.net>
2020-09-10 12:39:41 +0200

    ja no tenim cap squid 2.5

--- puppet/modules/squid/manifests/init.pp
+++ puppet/modules/squid/manifests/init.pp
@@ -2,11 +2,6 @@
 #
 class squid {
   include common::squid
-  $squid_version = $operatingsystem  ? {
-    Fedora => "2.5",
-    Debian => "2.7", #TODO: Debian has squid3 in stable repository
-    default => "2.6"
-  }
   file {
     "/etc/squid/squid.conf":
       mode => '0640',
