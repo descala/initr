ede34ebb9093a63234e5a487baba2bb50c5a261d
Lluís Gili <lluis@ingent.net>
2021-03-29 13:14:23 +0200

    suport per backup de diferents clusters

--- puppet/modules/postgres/files/dump_all.sh
+++ puppet/modules/postgres/files/dump_all.sh
@@ -1,20 +1,30 @@
 #! /bin/bash
 
+LSCLUSTERS='/usr/bin/pg_lsclusters'
 DUMPALL='/usr/bin/pg_dumpall'
 PSQL='/usr/bin/psql'
 DIR='/var/backups/postgres'
 mkdir -p "$DIR"
 cd "$DIR"
 
-# get list of databases in system , exclude the tempate dbs
-DBS=( $($PSQL --list --tuples-only |
-awk '!/template[01]/ && $1 != "|" {print $1}') )
+# list postgres clusters
+CLUSTERS=$( $LSCLUSTERS -h | grep online | tr -s ' ' | cut -d" " -f 2,3 --output-delimiter=',' )
 
-# next dump globals (roles and tablespaces) only
-$DUMPALL --globals-only  > "$DIR/globals"
+for l in $CLUSTERS ; do
 
-# now loop through each individual database and backup the
-# schema and data separately
-for database in "${DBS[@]}" ; do
-    pg_dump -f "$DIR/$database.backup" -F c -w "$database"
+  cluster=$(echo $l | cut -d"," -f1)
+  port=$(echo $l | cut -d"," -f2)
+
+  # get list of databases in cluster, exclude the tempate dbs
+  DBS=( $($PSQL -p $port --list --tuples-only | awk '!/template[01]/ && $1 != "|" {print $1}') )
+
+  # next dump globals (roles and tablespaces) only
+  $DUMPALL --globals-only  > "$DIR/globals_$cluster"
+
+  # now loop through each individual database and backup the
+  # schema and data separately
+  for database in "${DBS[@]}" ; do
+    pg_dump -p $port -f "$DIR/$database.$cluster.backup" -F c -w "$database"
+  done
+
 done
