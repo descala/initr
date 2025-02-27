005f50f793988aae7d4ebaf401f1a7c3def803bc
David Escala <descala@ingent.net>
2025-02-27 09:15:32 +0100

    Zen is no longer accessible. Use junkemailfilter

--- puppet/modules/nagios/manifests/check_blacklisted.pp
+++ puppet/modules/nagios/manifests/check_blacklisted.pp
@@ -9,7 +9,7 @@ class nagios::check_blacklisted {
     # Define ip on initr
     nagios::nsca_node::wrapper_check {
       "blacklisted":
-        command => "$nagios_plugins_dir/check_bl -H $check_bl_ip zen.spamhaus.org",
+        command => "$nagios_plugins_dir/check_bl -H $check_bl_ip hostkarma.junkemailfilter.com",
         minute => "*/30",
         freshness => "6000";
     }
@@ -18,7 +18,7 @@ class nagios::check_blacklisted {
       # Dynamic IP address with dyndns module
       nagios::nsca_node::wrapper_check {
         "blacklisted":
-          command => "$nagios_plugins_dir/check_bl -H $fqdn.$ddns_domain zen.spamhaus.org",
+          command => "$nagios_plugins_dir/check_bl -H $fqdn.$ddns_domain hostkarma.junkemailfilter.com",
           minute => "*/30",
           freshness => "6000";
       }
@@ -26,7 +26,7 @@ class nagios::check_blacklisted {
       # Host with static ip_address
       nagios::nsca_node::wrapper_check {
         "blacklisted":
-          command => "$nagios_plugins_dir/check_bl -H $ipaddress_internet zen.spamhaus.org",
+          command => "$nagios_plugins_dir/check_bl -H $ipaddress_internet hostkarma.junkemailfilter.com",
           minute => "*/30",
           freshness => "6000";
       }
